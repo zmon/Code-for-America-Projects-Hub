f4c026c763cd1dbbf388c67b39383f1a5b9ec216
Jason Stirnaman <stirnamanj@gmail.com>
2015-04-06 14:42:37 -0500

    Set dependency paths in index.html to use Bower-managed dependencies. Fixed typos in karma.conf.js.

--- karma.conf.js
+++ karma.conf.js
@@ -21,7 +21,7 @@ module.exports = function(config) {
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-mocks/angular-mocks.js',
-      'bower_components/angular-route/angular-scenarios.js',
+      'bower_components/angular-scenario/angular-scenario.js',
 
       // App
       'ng/*.js',
