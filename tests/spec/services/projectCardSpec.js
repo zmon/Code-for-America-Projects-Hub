62d836d2fba2119d95a8d11882912b90b189a2c4
Jason Stirnaman <stirnamanj@gmail.com>
2015-04-09 12:07:21 -0500

    Added service to provide data to projectCard controller.

--- tests/spec/services/projectCardSpec.js
+++ tests/spec/services/projectCardSpec.js
@@ -1,9 +1,25 @@
-describe("ProjectCard objects", function() {
-  // Initialization of the AngularJS application before each test case
-  beforeEach(module('cfahubApp'));
+describe("ProjectCardService", function() {
+  var $projectCards;
+  // Initialize the module before each test case
+  beforeEach(module('cfahubServices', function($provide) {
+    cfaApi = {};
+    // Add some mock data from Code for America API
+    cfaApi.projects = [];
+    cfaApi.projects[0] = {};
+    cfaApi.projects[0].title = "Project Title";
+    $provide.value('cfaApi', cfaApi);
+  }));
 
-  it("should be of type ProjectCard", function() {
-    var project = new cfahubApp.service('ProjectService',[]);
-    expect(project.projectCard).toBe(projectCard);
+  beforeEach(inject(function(ProjectCardService) {
+    $projectCards = ProjectCardService;
+  }));
+
+  it("should list ProjectCards", function() {
+    var cards = $projectCards.list();
+    expect(cards[0].projecttype).toEqual("project");
+  });
+
+  it("should assemble a card from cfaApi", function() {
+
   });
 });
