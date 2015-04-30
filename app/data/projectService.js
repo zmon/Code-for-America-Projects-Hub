f0c2e7096c00d67a5118eb2867c368623e6baccd
Jason Stirnaman <stirnamanj@gmail.com>
2015-04-30 01:28:37 -0500

    Fixed single project view, /project/[pid] by adding getProject() to ProjectService. Updated attribute names in mock project data to match ProjectIdeas.

--- app/data/projectService.js
+++ app/data/projectService.js
@@ -3,35 +3,44 @@ cfahubServices.factory('ProjectService', projectservice);
 projectservice.$inject = ['CFAProjectsService'];
 
 function projectservice(CFAProjectsService) {
-   return {
-      getProjects: getProjects
-      //getProject: getProject(projectId)
+    return {
+      getProjects: getProjects,
+      getProject: getProject
     };
 
-   function getProjects() {
-      return CFAProjectsService.getProjects()
+    function getProjects() {
+        return CFAProjectsService.getProjects()
         .then(getProjectsComplete)
         .catch(getProjectsFailed);
-   }
+    
+        function mergeProjectSources() {
+          // Merge external API objects into unified model
+        }
+    }
 
-   function getProjectsComplete(response) {
-      // return response.data.results;
-      mp = new mockProjects;
-      return mp.projects;
-   }
+    function getProject(projectId) {
+        var projects, project;
+        return getProjects().then(function(data) {
+            projects = data;
+            for (i = 0; i < projects.length; i++) {
+              project = projects[i];
+              if(project['id'] == projectId) {
+                return project;
+              }
+            }
+            return project;
+        });
+    }
+
+    function getProjectsComplete(response) {
+        // return response.data.results;
+        mp = new mockProjects;
+        return mp.projects;
+    }
 
    function getProjectsFailed(error) {
      //log an error
    }
-
-   // function getProject(projectId) {
-   //    var projects = getProjects;
-   //    for(i=0;i < projects.length;i++) {
-   //       if(projects()[i].id == projectId) {
-   //          return projects()[i];
-   //       }
-   //    }
-   // }
 
    function mockProjects() {
        this.projects = [{
@@ -43,10 +52,10 @@ function projectservice(CFAProjectsService) {
                 "subtitle":"Project Subtitle",
                 "category":"Civic category",
                 "goals":"The first paragraph from GitHb readme. Project goals. One paragraph of text describing why people need to use this app/website. The first four lines of text are displayed in the card view. The rest is available in details.",
-                "targetaudience":"1. New brigade members<br>2. Established brigade members<br>3. Brigades across the nation<br>4. Civic-minded public",
-                "userstories":"<p>Dave is a member of brigade living in Kansas City. He does not have time to follow the latest developments in the brigade on a daily basis. He would like to choose where he can apply his skills for a few hours during Monday hack night. Dave does not need to see project details, rather where the help is needed. He opens Projects Hub and chooses an active project with relevant Help Wanted issues, where he can contribute.</p><p>Ashley has a great idea about building up community spaces in Kansas City. She does not know how to build an app for it, and thinks Code for America might help. Ashley opens the Projects Hub, filters Community projects and sees that similar, but not quite the same project is under development in Munich, Germany. She presses the big blue button on the Projects Hub and adds the idea for KC brigade to develop.</p><p>Kevin, DB admin at MicroBucks, Inc., has found out about Code for America on a date with Ashley. He is curious about Ashley and the idea behind civic coding, and would like to learn more about both. He also would like to improve his JavaScript skills. Kevin opens the ProjectsHub, finds all the projects in Kansas City, where Javascript help is wanted. One of them has Ashley as a contributor. Kevin chooses to work on this project.</p><p>Jim, is geomapping guru, has been out of work for a few months. He is looking for a challenging project to showcase his skills and to build up his portfolio. He finds one under development in Buenos Aires brigade.</p>",
-                "maintenanceplan":"<p>Project leaders for active projects will be able to keep the the data updated on GitHub, using the usual GitHub tools. Additional requirement for GitHub is making custom set of GitHub repo labels (see <a href='https://github.com/codeforkansascity/Code-for-America-Projects-Hub/issues/8'>Custom Labels for Repos</a> on GitHub).  </p><p>Project Definition information can be updated either via Google form, when the project is created, or via Google miniforms, once project is established, or by editing Google spreasheet directly.</p>",
-                "blockers":"<p>Maintanence related blockers: The workflow for updating project information in Google spreadsheet is clumsy (Google form does not pull data back from the spreadsheet, if data exists already).</p><p>The initial effort of creating custom repo labels and then applying them to repo issue, when the issue is created requires some discipline from project leader. </p>",
+                "target_audiences":"1. New brigade members<br>2. Established brigade members<br>3. Brigades across the nation<br>4. Civic-minded public",
+                "user_stories":"<p>Dave is a member of brigade living in Kansas City. He does not have time to follow the latest developments in the brigade on a daily basis. He would like to choose where he can apply his skills for a few hours during Monday hack night. Dave does not need to see project details, rather where the help is needed. He opens Projects Hub and chooses an active project with relevant Help Wanted issues, where he can contribute.</p><p>Ashley has a great idea about building up community spaces in Kansas City. She does not know how to build an app for it, and thinks Code for America might help. Ashley opens the Projects Hub, filters Community projects and sees that similar, but not quite the same project is under development in Munich, Germany. She presses the big blue button on the Projects Hub and adds the idea for KC brigade to develop.</p><p>Kevin, DB admin at MicroBucks, Inc., has found out about Code for America on a date with Ashley. He is curious about Ashley and the idea behind civic coding, and would like to learn more about both. He also would like to improve his JavaScript skills. Kevin opens the ProjectsHub, finds all the projects in Kansas City, where Javascript help is wanted. One of them has Ashley as a contributor. Kevin chooses to work on this project.</p><p>Jim, is geomapping guru, has been out of work for a few months. He is looking for a challenging project to showcase his skills and to build up his portfolio. He finds one under development in Buenos Aires brigade.</p>",
+                "maintenance_plan":"<p>Project leaders for active projects will be able to keep the the data updated on GitHub, using the usual GitHub tools. Additional requirement for GitHub is making custom set of GitHub repo labels (see <a href='https://github.com/codeforkansascity/Code-for-America-Projects-Hub/issues/8'>Custom Labels for Repos</a> on GitHub).  </p><p>Project Definition information can be updated either via Google form, when the project is created, or via Google miniforms, once project is established, or by editing Google spreasheet directly.</p>",
+                "potential_blockers":"<p>Maintenance related blockers: The workflow for updating project information in Google spreadsheet is clumsy (Google form does not pull data back from the spreadsheet, if data exists already).</p><p>The initial effort of creating custom repo labels and then applying them to repo issue, when the issue is created requires some discipline from project leader. </p>",
                 "stars":"#",
                 "followers":"#",
                 "comments":"#",
@@ -69,10 +78,10 @@ function projectservice(CFAProjectsService) {
                 "subtitle":"Map of projects funded by Community Capital Fund",
                 "category":"Community",
                 "goals":"The Community Capital Fund and our partners have realized through our work that a map of existing community-based projects does not exist and that often, neighborhood associations and organizations are not aware of projects/programs occurring near them, and often do not know of other organizations doing similar work. CCF believes that neighborhoods that partner with and learn from others doing similar work will have greater capacity, opportunities, and resources to carry out their work; will have a greater impact on community development and revitalization than when working in a silo; and will have a more sustained, long-term impact.",
-                "targetaudience":"1. New brigade members<br>2. Established brigade members<br>3. Brigades across the nation<br>4. Civic-minded public",
-                "userstories":"<p>Dave is a member of brigade living in Kansas City. He does not have time to follow the latest developments in the brigade on a daily basis. He would like to choose where he can apply his skills for a few hours during Monday hack night. Dave does not need to see project details, rather where the help is needed. He opens Projects Hub and chooses an active project with relevant Help Wanted issues, where he can contribute.</p><p>Ashley has a great idea about building up community spaces in Kansas City. She does not know how to build an app for it, and thinks Code for America might help. Ashley opens the Projects Hub, filters Community projects and sees that similar, but not quite the same project is under development in Munich, Germany. She presses the big blue button on the Projects Hub and adds the idea for KC brigade to develop.</p><p>Kevin, DB admin at MicroBucks, Inc., has found out about Code for America on a date with Ashley. He is curious about Ashley and the idea behind civic coding, and would like to learn more about both. He also would like to improve his JavaScript skills. Kevin opens the ProjectsHub, finds all the projects in Kansas City, where Javascript help is wanted. One of them has Ashley as a contributor. Kevin chooses to work on this project.</p><p>Jim, is geomapping guru, has been out of work for a few months. He is looking for a challenging project to showcase his skills and to build up his portfolio. He finds one under development in Buenos Aires brigade.</p>",
-                "maintenanceplan":"<p>Project leaders for active projects will be able to keep the the data updated on GitHub, using the usual GitHub tools. Additional requirement for GitHub is making custom set of GitHub repo labels (see <a href='https://github.com/codeforkansascity/Code-for-America-Projects-Hub/issues/8'>Custom Labels for Repos</a> on GitHub).  </p><p>Project Definition information can be updated either via Google form, when the project is created, or via Google miniforms, once project is established, or by editing Google spreasheet directly.</p>",
-                "blockers":"<p>Maintanence related blockers: The workflow for updating project information in Google spreadsheet is clumsy (Google form does not pull data back from the spreadsheet, if data exists already).</p><p>The initial effort of creating custom repo labels and then applying them to repo issue, when the issue is created requires some discipline from project leader. </p>",
+                "target_audiences":"1. New brigade members<br>2. Established brigade members<br>3. Brigades across the nation<br>4. Civic-minded public",
+                "user_stories":"<p>Dave is a member of brigade living in Kansas City. He does not have time to follow the latest developments in the brigade on a daily basis. He would like to choose where he can apply his skills for a few hours during Monday hack night. Dave does not need to see project details, rather where the help is needed. He opens Projects Hub and chooses an active project with relevant Help Wanted issues, where he can contribute.</p><p>Ashley has a great idea about building up community spaces in Kansas City. She does not know how to build an app for it, and thinks Code for America might help. Ashley opens the Projects Hub, filters Community projects and sees that similar, but not quite the same project is under development in Munich, Germany. She presses the big blue button on the Projects Hub and adds the idea for KC brigade to develop.</p><p>Kevin, DB admin at MicroBucks, Inc., has found out about Code for America on a date with Ashley. He is curious about Ashley and the idea behind civic coding, and would like to learn more about both. He also would like to improve his JavaScript skills. Kevin opens the ProjectsHub, finds all the projects in Kansas City, where Javascript help is wanted. One of them has Ashley as a contributor. Kevin chooses to work on this project.</p><p>Jim, is geomapping guru, has been out of work for a few months. He is looking for a challenging project to showcase his skills and to build up his portfolio. He finds one under development in Buenos Aires brigade.</p>",
+                "maintenance_plan":"<p>Project leaders for active projects will be able to keep the the data updated on GitHub, using the usual GitHub tools. Additional requirement for GitHub is making custom set of GitHub repo labels (see <a href='https://github.com/codeforkansascity/Code-for-America-Projects-Hub/issues/8'>Custom Labels for Repos</a> on GitHub).  </p><p>Project Definition information can be updated either via Google form, when the project is created, or via Google miniforms, once project is established, or by editing Google spreasheet directly.</p>",
+                "potential_blockers":"<p>Maintenance related blockers: The workflow for updating project information in Google spreadsheet is clumsy (Google form does not pull data back from the spreadsheet, if data exists already).</p><p>The initial effort of creating custom repo labels and then applying them to repo issue, when the issue is created requires some discipline from project leader. </p>",
                 "stars":"1",
                 "followers":"7",
                 "comments":"0",
@@ -106,10 +115,10 @@ function projectservice(CFAProjectsService) {
                 "subtitle":"and where you can help",
                 "category":"Code for America Brigade Tool",
                 "goals":"To help brigade members to find projects to work on. Multi-brigade collaboration. New members onboarding. Let public contribute ideas. Karma. Code for America scope view. Provide filtered and sorted card list of projects and ideas for development. The list should be available nation-wide and by city.",
-                "targetaudience":"1. New brigade members<br>2. Established brigade members<br>3. Brigades across the nation<br>4. Civic-minded public",
-                "userstories":"<p>Dave is a member of brigade living in Kansas City. He does not have time to follow the latest developments in the brigade on a daily basis. He would like to choose where he can apply his skills for a few hours during Monday hack night. Dave does not need to see project details, rather where the help is needed. He opens Projects Hub and chooses an active project with relevant Help Wanted issues, where he can contribute.</p><p>Ashley has a great idea about building up community spaces in Kansas City. She does not know how to build an app for it, and thinks Code for America might help. Ashley opens the Projects Hub, filters Community projects and sees that similar, but not quite the same project is under development in Munich, Germany. She presses the big blue button on the Projects Hub and adds the idea for KC brigade to develop.</p><p>Kevin, DB admin at MicroBucks, Inc., has found out about Code for America on a date with Ashley. He is curious about Ashley and the idea behind civic coding, and would like to learn more about both. He also would like to improve his JavaScript skills. Kevin opens the ProjectsHub, finds all the projects in Kansas City, where Javascript help is wanted. One of them has Ashley as a contributor. Kevin chooses to work on this project.</p><p>Jim, is geomapping guru, has been out of work for a few months. He is looking for a challenging project to showcase his skills and to build up his portfolio. He finds one under development in Buenos Aires brigade.</p>",
-                "maintenanceplan":"<p>Project leaders for active projects will be able to keep the the data updated on GitHub, using the usual GitHub tools. Additional requirement for GitHub is making custom set of GitHub repo labels (see <a href='https://github.com/codeforkansascity/Code-for-America-Projects-Hub/issues/8'>Custom Labels for Repos</a> on GitHub).  </p><p>Project Definition information can be updated either via Google form, when the project is created, or via Google miniforms, once project is established, or by editing Google spreasheet directly.</p>",
-                "blockers":"<p>Maintanence related blockers: The workflow for updating project information in Google spreadsheet is clumsy (Google form does not pull data back from the spreadsheet, if data exists already).</p><p>The initial effort of creating custom repo labels and then applying them to repo issue, when the issue is created requires some discipline from project leader. </p>",
+                "target_audiences":"1. New brigade members<br>2. Established brigade members<br>3. Brigades across the nation<br>4. Civic-minded public",
+                "user_stories":"<p>Dave is a member of brigade living in Kansas City. He does not have time to follow the latest developments in the brigade on a daily basis. He would like to choose where he can apply his skills for a few hours during Monday hack night. Dave does not need to see project details, rather where the help is needed. He opens Projects Hub and chooses an active project with relevant Help Wanted issues, where he can contribute.</p><p>Ashley has a great idea about building up community spaces in Kansas City. She does not know how to build an app for it, and thinks Code for America might help. Ashley opens the Projects Hub, filters Community projects and sees that similar, but not quite the same project is under development in Munich, Germany. She presses the big blue button on the Projects Hub and adds the idea for KC brigade to develop.</p><p>Kevin, DB admin at MicroBucks, Inc., has found out about Code for America on a date with Ashley. He is curious about Ashley and the idea behind civic coding, and would like to learn more about both. He also would like to improve his JavaScript skills. Kevin opens the ProjectsHub, finds all the projects in Kansas City, where Javascript help is wanted. One of them has Ashley as a contributor. Kevin chooses to work on this project.</p><p>Jim, is geomapping guru, has been out of work for a few months. He is looking for a challenging project to showcase his skills and to build up his portfolio. He finds one under development in Buenos Aires brigade.</p>",
+                "maintenance_plan":"<p>Project leaders for active projects will be able to keep the the data updated on GitHub, using the usual GitHub tools. Additional requirement for GitHub is making custom set of GitHub repo labels (see <a href='https://github.com/codeforkansascity/Code-for-America-Projects-Hub/issues/8'>Custom Labels for Repos</a> on GitHub).  </p><p>Project Definition information can be updated either via Google form, when the project is created, or via Google miniforms, once project is established, or by editing Google spreasheet directly.</p>",
+                "potential_blockers":"<p>Maintenance related blockers: The workflow for updating project information in Google spreadsheet is clumsy (Google form does not pull data back from the spreadsheet, if data exists already).</p><p>The initial effort of creating custom repo labels and then applying them to repo issue, when the issue is created requires some discipline from project leader. </p>",
                 "stars":"2",
                 "followers":"8",
                 "comments":"1",
--- app/project/ProjectController.js
+++ app/project/ProjectController.js
@@ -1,12 +1,16 @@
 cfahubControllers.controller('ProjectController', function($scope, $routeParams, ProjectService) {
     var projectId = $routeParams.pid;
-    $scope.project = ProjectService.getProject(projectId);
+    return ProjectService.getProject(projectId)
+    .then(function(data) {
+        $scope.project = data;
+        return $scope.project;
+    });
 });
 
 cfahubControllers.controller('ProjectListController', function($scope, $routeParams, ProjectService) {
-          return ProjectService.getProjects()
-          .then(function(data) {
-              $scope.ideas = data;
-              return $scope.ideas;
-      });
+    return ProjectService.getProjects()
+    .then(function(data) {
+        $scope.projects = data;
+        return $scope.projects;
+    });
 });
