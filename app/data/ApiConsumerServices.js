911fc2071bf28c103cf4e6c672bf66f7505f5401
Jason Stirnaman <stirnamanj@gmail.com>
2015-05-01 03:18:31 -0500

    Projects are dynamic, coming from merged CFA and Github API records. The merging is simplistic and shallow. Matching keys, e.g. ID, will get overwritten by one record. No attempt at a common schema yet. I'm  using promise chaining to get the data from the API services, as described at http://solutionoptimist.com/2013/12/27/javascript-promise-chains-2/. Added a search filter to Data.html.

--- app/data/ApiConsumerServices.js
+++ app/data/ApiConsumerServices.js
@@ -6,21 +6,19 @@ cfahubServices.factory('CFAProjectsService', CFAProjectsService);
 CFAProjectsService.$inject = ['$http'];
 
 function CFAProjectsService($http) {
-    return {
-      getProjects: getProjects
-    };
+  return {
+    getProjects: getProjects
+  };
+  //@TODO CFA API returns paged results in sets of 10.
+  function getProjects() {
+    return $http.get('http://codeforamerica.org/api/organizations/Code-for-Kansas-City/projects')
+      .success(getProjectsComplete)
+      .error(serviceError);
 
-    function getProjects() {
-      return $http.get('http://codeforamerica.org/api/organizations/Code-for-Kansas-City/projects')
-        .success(getProjectsComplete)
-        .error(serviceError);
-
-      function getProjectsComplete(data, status) {
-          return data.objects;
-        }
-
-      serviceError(data, status, statusText);
+    function getProjectsComplete(data, status) {
+      return data.objects;
     }
+  }
 }
 
 cfahubServices.factory('GithubProjectsService', GithubProjectsService);
@@ -28,21 +26,19 @@ cfahubServices.factory('GithubProjectsService', GithubProjectsService);
 GithubProjectsService.$inject = ['$http'];
 
 function GithubProjectsService($http) {
-    return {
-      getProjects: getProjects
-    };
+  return {
+    getProjects: getProjects
+  };
 
-    function getProjects() {
-      return $http.get('https://api.github.com/orgs/codeforkansascity/repos')
-        .success(getProjectsComplete)
-        .error(serviceError);
+  function getProjects() {
+    return $http.get('https://api.github.com/orgs/codeforkansascity/repos')
+      .success(getProjectsComplete)
+      .error(serviceError);
 
-      function getProjectsComplete(data, status) {
-        return data;
-        }
-
-      serviceError(data, status, statusText);
+    function getProjectsComplete(data, status) {
+      return data;
     }
+  }
 }
 
 cfahubServices.factory('GoogleProjectsService', GoogleProjectsService);
@@ -50,19 +46,15 @@ cfahubServices.factory('GoogleProjectsService', GoogleProjectsService);
 GoogleProjectsService.$inject = ['$http'];
 
 function GoogleProjectsService($http) {
-    return {
-      getApprovedProjects: getApprovedProjects
-    };
+  return {
+    getApprovedProjects: getApprovedProjects
+  };
 
-    function getApprovedProjects() {
-      return $http.get('https://spreadsheets.google.com/feeds/worksheets/1tnW2fTcPEQG93oebrCfvjZw4Vjtn6vkzvqyovxebKlI/public/full?alt=json')
-       .success(getFeedItems)
-       .error(serviceError);
-
-      getFeedItems(data, status);
-
-      serviceError(data, status, statusText);
-    }
+  function getApprovedProjects() {
+    return $http.get('https://spreadsheets.google.com/feeds/worksheets/1tnW2fTcPEQG93oebrCfvjZw4Vjtn6vkzvqyovxebKlI/public/full?alt=json')
+      .success(getFeedItems)
+      .error(serviceError);
+  }
 }
 
 cfahubServices.factory('GoogleProjectIdeasService', GoogleProjectIdeasService);
@@ -70,19 +62,15 @@ cfahubServices.factory('GoogleProjectIdeasService', GoogleProjectIdeasService);
 GoogleProjectIdeasService.$inject = ['$http'];
 
 function GoogleProjectIdeasService($http) {
-    return {
-      getSubmittedIdeas: getSubmittedIdeas
-    };
+  return {
+    getSubmittedIdeas: getSubmittedIdeas
+  };
 
-    function getSubmittedIdeas() {
-      return $http.get('https://spreadsheets.google.com/feeds/list/1PGM2P9o0bkJ_xCkoH2ps_Dp5xnBDrPxmIB-jnJWAwhE/1/public/values?alt=json')
-       .success(getFeedItems)
-       .error(serviceError);
-
-      feedItems(data, status);
-
-      serviceError(data, status, statusText);
-    }
+  function getSubmittedIdeas() {
+    return $http.get('https://spreadsheets.google.com/feeds/list/1PGM2P9o0bkJ_xCkoH2ps_Dp5xnBDrPxmIB-jnJWAwhE/1/public/values?alt=json')
+      .success(getFeedItems)
+      .error(serviceError);
+  }
 }
 
 function getFeedItems(data, status) {
