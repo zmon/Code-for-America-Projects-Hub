b244047578924e65dd3f55de70643543e5918418
Paul D Barham <paulb@savagesoft.com>
2015-06-13 19:11:50 -0500

    Code reformated

--- app/hub/HubController.js
+++ app/hub/HubController.js
@@ -1,22 +1,23 @@
 cfahubControllers.controller('HubController',
     ['$scope', 'ProjectService', 'ProjectIdeaService',
-      function($scope, ProjectService, ProjectIdeaService) {
-    $scope.cards = {};
+        function ($scope, ProjectService, ProjectIdeaService) {
+            $scope.cards = {};
 
-    ProjectService.getProjects()
-        .then(function(data) {
-            $scope.cards.projects = data;
-            return $scope.cards.projects;
-    });
-    
-    ProjectIdeaService.getIdeas()
-        .then(function(data) {
-            $scope.cards.ideas = data["App.Model.ProjectIdea"];
-            return $scope.cards.ideas;
-    });
-}]);
+            ProjectService.getProjects()
+                .then(function (data) {
+                    $scope.cards.projects = data;
+                    console.dir(data);
+                    return $scope.cards.projects;
+                });
+
+            ProjectIdeaService.getIdeas()
+                .then(function (data) {
+                    $scope.cards.ideas = data["App.Model.ProjectIdea"];
+                    return $scope.cards.ideas;
+                });
+        }]);
 
 cfahubControllers.controller('wrong404Ctrl', ['$scope',
-    function($scope) {
+    function ($scope) {
 
-}]);
+    }]);
